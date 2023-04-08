be7965170dc4634b2ebff0bd9aed9ed346bf8026
Oluwatosin Rhoda Adeleke <Adeleke.O@bullseye.local>
2023-04-08 12:36:46 +0100

    Practice With Godwin

---
+++ Image.html/Card.html/index.js
@@ -0,0 +1,22 @@
+// let areaOfSquare = lenght * breadth;
+// let lenghOfsquare = length squared;
+
+// console.log(areaOfSquare
+// )
+
+
+// let base = 10
+// let height =20
+// let triarea = (1/2) * base * height
+// console.log(triarea);
+
+// let a = 3
+// let p = 2
+// let r = 4
+// let n = 6
+// let t = 7
+// let CI = a = p*(1+r/n)**n*t
+// console.log(CI);
+//  myName = "Oluwatosin"
+// var yourName = prompt ("What is your name?");
+// alert("My name is " + myName + ",Welcome to my course" + yourName + "!");var
